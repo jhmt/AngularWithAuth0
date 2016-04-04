474c01954918cf21ec43b8e2e0765be91e197cc0
jhmt <jhashimot00@gmail.com>
2016-04-05 10:23:57 +1200

    Add ShipmentsAPI

---
+++ ShipmentsAPI/ShipmentsAPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using ShipmentsAPI;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace ShipmentsAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new System.Web.Http.HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
